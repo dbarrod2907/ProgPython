c7ed9be5caab87ed50a7f1ada247cf2a9a92a145
dbarrod2907 <150685116+dbarrod2907@users.noreply.github.com>
2023-11-20 15:00:13 +0100

    Add files via upload

---
+++ tarea3/Tarea3_1.py
@@ -0,0 +1,18 @@
+num1=int(input("Introduce primer numero:"))
+num2=int(input("Introduce segundo numero:"))
+num3=int(input("Introduce tercer numero:"))
+if num1>num2 and num1>num3
+print("El mayor es ",num1)
+else:
+    if num2>num3:
+        print("El mayor es ",num2)
+        else:
+            print("El mayor es ", num3)
+            
+if num1<num2 and num1<num3
+print("El menor es ",num1)
+else:
+    if num2>num3:
+        print("El menor es ",num2)
+        else:
+            print("El menor es ", num3)
---
+++ tarea3/Tarea3_3.py
@@ -0,0 +1,11 @@
+cordX=int(input("Introduce coordenada X:))
+cordY=int(input("Introduce coordenada Y:))
+if cordX>0 and cordY>0:
+                print("Primer cuadrante")
+if cordX<0 and cordY>0:
+                print("Segundo cuadrante")
+if cordX<0 and cordY<0:
+                print("Tercer cuadrante")
+if cordX>0 and cordY<0:
+                print("Cuarto cuadrante")
+

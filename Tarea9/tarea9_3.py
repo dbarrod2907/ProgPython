6ced39b81c8442cc4e21075de129fd864ceafadb
dbarrod2907 <150685116+dbarrod2907@users.noreply.github.com>
2024-01-09 12:52:32 +0100

    Add files via upload

---
+++ Tarea9/tarea9_3.py
@@ -0,0 +1,11 @@
+#Definir lista vacia
+lista=[]
+#Agregamos 5 nombres
+for x in range(5)
+valor=input("Introduce un nombre:")
+#Visualizar cuantos tienen 5 o mas caracteres
+for x in range(5):
+    if len(lista[x])>=5:
+        print(lista[x],end=" ")
+
+print()
